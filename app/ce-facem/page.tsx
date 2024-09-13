7314c277ef3b9c1788eeeaefbb4d2c540fc77672
mares-gabriel <maresgabriel.dev@gmail.com>
2024-09-13 21:13:12 +0300

    added the ce facem page first section

--- app/ce-facem/page.tsx
+++ app/ce-facem/page.tsx
@@ -1,5 +1,12 @@
+// Styles
+import styles from "./page.module.scss";
+
 // Components
 import PageTitle from "@/components/pageTitle/pageTitle";
+import BucurestiulDupaCutremur from "@/components/ce-facem/bucurestiulDupaCutremur/bucurestiulDupaCutremur";
+import OrdinulCriminal from "@/components/ce-facem/ordinulCriminal/ordinulCriminal";
+import MemorialulLuiUrsu from "@/components/ce-facem/memorialulLuiUrsu/memorialulLuiUrsu";
+import Implicate from "@/components/homepage/implica-te/implicate";
 
 export default function CeFacem() {
    return (
@@ -8,6 +15,12 @@ export default function CeFacem() {
             title="Ce facem în Re:Rise?"
             description="Re:rise este o asociație care reduce vulnerabilitatea seismică în România, conectând persoanele expuse riscului cu resursele disponibile. Organizația implementează 5 soluții, inclusiv un Registru Digital al Utilajelor pentru forțele de intervenție, un Sistem de Mapare Rapidă cu Drone pentru autorități și WiFi Mesh-ul Salvatorului pentru comunicarea între salvatori și victime."
          />
+         <BucurestiulDupaCutremur />
+         <OrdinulCriminal />
+         <MemorialulLuiUrsu />
+         <div className={styles.projects}></div>
+
+         <Implicate />
       </div>
    );
 }
---
+++ components/ce-facem/ordinulCriminal/ordinulCriminal.tsx
@@ -0,0 +1,44 @@
+// Styles
+import styles from "./ordinulCriminal.module.scss";
+
+// Components
+import Image from "next/image";
+import Title from "@/components/title/title";
+import Paragraph from "@/components/paragraph/paragraph";
+import Button from "@/components/button/button";
+import Link from "next/link";
+
+// Assets
+import poster from "../../../assets/ordinulCriminal.png";
+
+export default function OrdinulCriminal() {
+   return (
+      <div className={styles.container}>
+         <Image src={poster} alt="" />
+         <div>
+            <Title size="small">Ordinul Criminal</Title>
+            <Paragraph>
+               La 4 luni după cutremurul din martie 1977, Nicolae Ceaușescu
+               convoacă o ședință urgentă în București. Pe 4 iulie, cca. 300 de
+               persoane (ingineri, arhitecți, lideri de partid și ai
+               administrației locale), sunt strânși într-o sală din spatele CC
+               și li se ordonă de către Ceaușescu să sisteze toate lucrările de
+               consolidare la clădirile afectate de cutremur.
+               <br />
+               <br />
+               Numai în Capitală, peste 10.000 de clădiri au fost lăsate astfel
+               în vulnerabilitate.
+               <br />
+               <br />
+               Re:Rise publică în premieră stenograma acelei ședințe și dovezile
+               ordinului criminal.
+            </Paragraph>
+            <Link href={""}>
+               <Button size="medium" type="secondary">
+                  <Paragraph>Află mai multe</Paragraph>
+               </Button>
+            </Link>
+         </div>
+      </div>
+   );
+}
